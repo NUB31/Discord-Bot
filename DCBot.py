43f6db3c4e205f9f9c0f709e608287b2400dbeaa
Oliver Stene <oste@2ISF.alesund.vgs.no>
2021-09-22 10:53:28 +0200

    salam

---
+++ DCBot.py
@@ -0,0 +1,33 @@
+import discord
+from discord.state import ConnectionState
+import requests
+import json
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        messageContent = message.content
+        messageContentLower = messageContent.lower()
+        if("vanylven" in messageContentLower and self.user != message.author or "fiskå" in messageContentLower and self.user != message.author):
+            response = requests.get('https://api.giphy.com/v1/gifs/random?api_key=mz2Z5lXfYQZr7x5Hk8OCKKUyNyCctiBq&rating=unrated&tag=laugh')
+            response = response.json()
+            # print(response["data"])
+            embed = discord.Embed(title="vanylven shit lol")
+            embed.set_image(url=str(response["data"]['images']['downsized_large']["url"]))
+            await message.channel.send(embed=embed)
+        if message.content.startswith("!uwu "):
+            print(message.content)
+            word = []
+            for i in range(len(message.content)):
+                if (i > 4):
+                    word.append(message.content[i])
+            x = "".join(word)
+            response = requests.get('http://localhost:3000/uwu?text=' + x)
+            response = response.json()
+            await message.channel.send(response["Text"])
+
+
+client = MyClient()
+client.run('ODg5NTM3MDUzMjkxODU1OTQz.YUir8A.orzVHO5OyxoBB10YUs0OEmXBMvQ')
